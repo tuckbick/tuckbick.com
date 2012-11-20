448dddefdf7e6f8ef44e0c6c73444a2006e3c569
Tucker Bickler <tuckersworld@gmail.com>
2012-11-20 15:21:55 -0600

    added project scaffolding

---
+++ app/Controller/ProjectsController.php
@@ -0,0 +1,16 @@
+<?php
+App::uses('AppController', 'Controller');
+/**
+ * Projects Controller
+ *
+ */
+class ProjectsController extends AppController {
+
+/**
+ * Scaffold
+ *
+ * @var mixed
+ */
+	public $scaffold;
+
+}
---
+++ app/Test/Case/Controller/ProjectsControllerTest.php
@@ -0,0 +1,19 @@
+<?php
+App::uses('ProjectsController', 'Controller');
+
+/**
+ * ProjectsController Test Case
+ *
+ */
+class ProjectsControllerTest extends ControllerTestCase {
+
+/**
+ * Fixtures
+ *
+ * @var array
+ */
+	public $fixtures = array(
+		'app.project'
+	);
+
+}
