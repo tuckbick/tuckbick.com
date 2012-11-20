c374f0ee1b1403506517380f24738f47aa48cf6e
Tucker Bickler <tbickler@optaros.com>
2012-11-20 17:57:54 -0600

    updated Project fixture, added Project src_url notempty validation criteria

--- app/Model/Project.php
+++ app/Model/Project.php
@@ -64,6 +64,14 @@ class Project extends AppModel {
 				//'last' => false, // Stop validation after this rule
 				//'on' => 'create', // Limit validation to 'create' or 'update' operations
 			),
+			'notempty' => array(
+				'rule' => array('notempty'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
 			'maxlength' => array(
 				'rule' => array('maxlength', 200),
 				//'message' => 'Your custom message here',
--- app/Test/Fixture/ProjectFixture.php
+++ app/Test/Fixture/ProjectFixture.php
@@ -13,8 +13,8 @@ class ProjectFixture extends CakeTestFixture {
 	public $fields = array(
 		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'length' => 10, 'key' => 'primary'),
 		'title' => array('type' => 'string', 'null' => true, 'default' => null, 'length' => 200, 'collate' => 'utf8_unicode_ci', 'charset' => 'utf8'),
-		'demo_url' => array('type' => 'string', 'null' => false, 'default' => null, 'length' => 500, 'collate' => 'utf8_unicode_ci', 'charset' => 'utf8'),
-		'src_url' => array('type' => 'string', 'null' => false, 'default' => null, 'length' => 500, 'collate' => 'utf8_unicode_ci', 'charset' => 'utf8'),
+		'demo_url' => array('type' => 'string', 'null' => false, 'default' => null, 'length' => 200, 'collate' => 'utf8_unicode_ci', 'charset' => 'utf8'),
+		'src_url' => array('type' => 'string', 'null' => false, 'default' => null, 'length' => 200, 'collate' => 'utf8_unicode_ci', 'charset' => 'utf8'),
 		'body' => array('type' => 'text', 'null' => true, 'default' => null, 'collate' => 'utf8_unicode_ci', 'charset' => 'utf8'),
 		'created' => array('type' => 'datetime', 'null' => true, 'default' => null),
 		'modified' => array('type' => 'datetime', 'null' => true, 'default' => null),
@@ -36,8 +36,8 @@ class ProjectFixture extends CakeTestFixture {
 			'demo_url' => 'Lorem ipsum dolor sit amet',
 			'src_url' => 'Lorem ipsum dolor sit amet',
 			'body' => 'Lorem ipsum dolor sit amet, aliquet feugiat. Convallis morbi fringilla gravida, phasellus feugiat dapibus velit nunc, pulvinar eget sollicitudin venenatis cum nullam, vivamus ut a sed, mollitia lectus. Nulla vestibulum massa neque ut et, id hendrerit sit, feugiat in taciti enim proin nibh, tempor dignissim, rhoncus duis vestibulum nunc mattis convallis.',
-			'created' => '2012-11-20 15:14:21',
-			'modified' => '2012-11-20 15:14:21'
+			'created' => '2012-11-20 17:56:51',
+			'modified' => '2012-11-20 17:56:51'
 		),
 	);
 
