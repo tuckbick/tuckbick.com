b77692b7f90ddac2bc0673ecbbf721e8e409ab15
Tucker Bickler <tuckersworld@gmail.com>
2026-09-10 22:42:00 -0500

    chore: adjust public dir

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ const favicon = document.querySelector<HTMLLinkElement>("link[rel*='icon']");
 if (favicon) {
   let idx = 1;
   setInterval(() => {
-    favicon.href = '/public/favicon' + idx + '.svg';
+    favicon.href = '/favicon' + idx + '.svg';
     idx = (idx + 1) % 3;
   }, 1000)
 }
