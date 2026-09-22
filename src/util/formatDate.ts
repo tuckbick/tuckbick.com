beb0cbd8456eab0907201a44543209443c47060e
Tucker Bickler <tuckersworld@gmail.com>
2026-09-22 17:05:49 -0500

    chore: fixing date formatting

--- src/util/formatDate.ts
+++ src/util/formatDate.ts
@@ -1,4 +1,4 @@
 export default (date: string) => {
     const d = new Date(date)
-    return d.toLocaleDateString('en-US', { month: 'short', year: 'numeric' }).replace(',', '')
+    return d.toLocaleDateString('en-US', { month: 'short', year: 'numeric', timeZone: 'UTC' }).replace(',', '')
 }
